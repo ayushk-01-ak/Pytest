78b9194f6650664424377aacef744b2f1300a87c
Ayush Kumar Sahoo <ayushkumarsahoo67@gmail.com>
2026-06-19 22:39:01 +0530

    Second commit

---
+++ test2.py
@@ -0,0 +1,60 @@
+#Arithmetic Operators
+# a=10
+# b=3
+# print("Addition:", a + b)
+# print("Subtraction:", a - b)
+# print("Multiplication:", a * b) 
+# print("Division:", a / b)
+# print("Floor Division:", a // b)
+# print("Modulus:", a % b)
+
+#Assignment Operators
+# a=10
+# b=10
+# a+=20 # a = a + 20
+# a+=b
+# print("a=", a)
+# a-=10
+# print("a=", a)
+# a*=10
+# print("a=", a)
+# a/=10
+# print("a=", a)
+# a%=10
+# print("a=", a)
+# a//=3
+# print("a=", a)
+
+#Logical Operators
+# a= True
+# b= False   
+# print("a and b:", a and b)
+# print("a or b:", a or b)
+# print("not a:", not a)
+
+
+#comparison Operators
+# a=10
+# b=20   
+# print("a==b:", a==b)
+# print("a!=b:", a!=b)
+# print("a>b:", a>b)
+# print("a<b:", a<b)
+# print("a>=b:", a>=b)
+# print("a<=b:", a<=b)
+
+#Membership Operators
+# first = [1, 2, 3, 4, 5, True, False, "Hello", "World"]
+# print("1 in first:", 1 in first)
+# print("6 in first:", 6 in first)
+# print("Hello in first:", "Hello" in first)
+# print("Python in first:", "Python" in first)
+
+#identity Operators
+# a=10
+# b=20
+# c = a
+# print("a is b:", a is b)
+# print("a is c:", a is c)
+# print("a is not b:", a is not b)
+# print("a is not c:", a is not c)
